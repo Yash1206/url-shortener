63e24fdd90780ace445ca87d4a0e4b2c008b28ab
yjspoder <injose.joshi@gmail.com>
2020-05-11 19:17:26 +0530

    Add Url model

--- db/migrate/20200511131639_create_url.rb
+++ db/migrate/20200511131639_create_url.rb
@@ -1,8 +1,10 @@
 class CreateUrl < ActiveRecord::Migration[6.0]
+  
   def change
     create_table :urls do |t|
       t.string :full, null: false
       t.string :shortened, null: false
     end
   end
+
 end
