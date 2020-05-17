5056c0c6c17215bbce23d0bf2455215f6624078a
yjspoder <injose.joshi@gmail.com>
2020-05-17 13:57:47 +0530

    Add resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post '/url' => "urls#encode"
-  get '/show' => "urls#decode"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :urls
+  post '/encode' => "urls#encode"
+  get '/decode' => "urls#decode"
 end
