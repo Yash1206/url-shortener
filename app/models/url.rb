a57d92c7938b952a25c14edbf84b110844af97f1
yjspoder <injose.joshi@gmail.com>
2020-05-19 22:45:08 +0530

    refactor url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,8 +1,8 @@
 class Url < ApplicationRecord
-  before_validation :hash
+  before_create :hash
   validates :original, presence: true, uniqueness: true
   validates_format_of :original, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
-  validates :shortened, presence: true, uniqueness: true
+  validates :shortened, presence: true #uniqueness: true
 
   private
 
