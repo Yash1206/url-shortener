fa01c3112cc896e7d586b2594db10b6533d8d15c
yjspoder <injose.joshi@gmail.com>
2020-05-20 02:38:25 +0530

    change hash to generate_slug in url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,12 +1,12 @@
 class Url < ApplicationRecord
-  before_create :hash
+  before_create :generate_shortened
   validates :original, presence: true, uniqueness: true
   validates_format_of :original, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
-  validates :shortened, presence: true #uniqueness: true
+  validates :shortened, presence: true, uniqueness: true
 
   private
 
-  def hash
+  def generate_shortened
     loop do
       self.shortened = SecureRandom.hex(4)
       break unless Url.exists?( shortened: self.shortened )
