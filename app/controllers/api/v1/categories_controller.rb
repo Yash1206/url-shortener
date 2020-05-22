372cc740a521b6710c8f2c1509045879a3d818bb
yjspoder <injose.joshi@gmail.com>
2020-05-22 15:05:49 +0530

    Add create action

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,12 +1,23 @@
 class Api::V1::CategoriesController < ApplicationController
   before_action :category_list, only: [:index]
+  before_action :category_params, only: [:create]
 
   def index
     render json: category_list
   end
 
   def create
-
+    @category = Category.find_by(category_params)
+    if @category
+      render status: :unprocessable_entity, json: { errors: "Provided category already exists, please input a new category." }
+    else
+      @category = Category.create(category_params)
+      if @category.save
+        render status: :ok, json: { category: @category }
+      else
+        render status: :unprocessable_entity, json: { errors: @category.errors.full_messages }
+      end
+    end
   end
 
   def update
@@ -25,5 +36,9 @@ class Api::V1::CategoriesController < ApplicationController
 
   def category_list
     Category.order(updated_at: :desc)
+  end
+
+  def category_params
+    params.require(:category).permit(:title)
   end
 end
