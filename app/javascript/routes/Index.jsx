8a6aa20e871ee739c416fefe958636dd1d652a0d
yjspoder <injose.joshi@gmail.com>
2020-05-23 19:28:40 +0530

    Add manage category route

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import List from "../components/List";
+import CategoryForm from "../components/CategoryForm";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={List} />
+      <Route path="/manage-categories" exact component={CategoryForm} />
     </Switch>
   </Router>
 );
