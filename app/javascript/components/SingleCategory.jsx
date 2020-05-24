6efe43dfd2b0688cde8d9b1e3d7143ed4d3ad766
yjspoder <injose.joshi@gmail.com>
2020-05-24 14:49:47 +0530

    Add single category component

---
+++ app/javascript/components/SingleCategory.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SingleCategory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Test</div>;
+  }
+}
+
+export default SingleCategory;
--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import List from "../components/List";
-import CategoryForm from "../components/CategoryForm";
+// import CategoryForm from "../components/CategoryForm";
+import SingleCategory from "../components/SingleCategory";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={List} />
-      <Route path="/manage-categories" exact component={CategoryForm} />
+      <Route path="/manage-categories" exact component={SingleCategory} />
     </Switch>
   </Router>
 );
