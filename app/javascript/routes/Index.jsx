c4639a242251d26713c2a5cddce5dd2fc583fc83
yjspoder <injose.joshi@gmail.com>
2020-05-25 09:25:10 +0530

    Add manage categories to Index routes

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import List from "../components/List";
-// import CategoryForm from "../components/CategoryForm";
-import SingleCategory from "../components/SingleCategory";
+import ManageCategories from "../components/ManageCategories";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={List} />
-      <Route path="/manage-categories" exact component={SingleCategory} />
+      <Route path="/manage-categories" exact component={ManageCategories} />
     </Switch>
   </Router>
 );
