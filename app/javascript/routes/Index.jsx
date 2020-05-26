4a24fbc77d9617cd87897b53fe6ee5b0dc810a08
yjspoder <injose.joshi@gmail.com>
2020-05-26 11:16:24 +0530

    move bootstrap imports

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ManageCategories from "../components/categories/ManageCategories";
 import ManageUrls from "../components/urls/ManageUrls";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 export default (
   <Router>
     <Switch>
