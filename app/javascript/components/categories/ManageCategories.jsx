059355134c7bb134b1e43f6c194f900d69538097
yjspoder <injose.joshi@gmail.com>
2020-05-26 06:30:12 +0530

    Add error handling to ManageCategories

--- app/javascript/components/categories/ManageCategories.jsx
+++ app/javascript/components/categories/ManageCategories.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import CategoryForm from "./CategoryForm";
 import CategoryList from "./CategoryList";
+import { Link } from "react-router-dom";
 
 class ManageCategories extends Component {
   constructor() {
     super();
     this.state = {
-      categoryList: null,
+      categoryList: [],
+      errors: [],
     };
   }
 
@@ -19,20 +21,42 @@ class ManageCategories extends Component {
     fetch(url)
       .then((response) => response.json())
       .then((response) => {
-        this.setState({ categoryList: response });
+        this.setState({ categoryList: response , errors: []});
       });
+  };
+
+  handleResponse = (response) => {
+    if (response.status == 200) {
+      this.loadCategories();
+    } else {
+      response.json().then((err) => {
+        this.setState({
+          errors: err.errors,
+        });
+      });
+    }
   };
 
   render() {
     return (
       <div className="container">
-        <CategoryForm loadCategories={this.loadCategories} />
+        <div className="d-flex justify-content-end">
+          <Link className="btn btn-link" to="/">
+            Manage Urls
+          </Link>
+        </div>
         <h1
           style={{ padding: "2rem" }}
           className="d-flex justify-content-center"
         >
           Category List
         </h1>
+        <CategoryForm
+          state={this.state}
+          loadCategories={this.loadCategories}
+          handleResponse={this.handleResponse}
+          errors
+        />
         <CategoryList
           categoryList={this.state.categoryList}
           loadCategories={this.loadCategories}
