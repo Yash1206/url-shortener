d7b45dd3628b556ada4de8ff332676b70bce000c
yjspoder <injose.joshi@gmail.com>
2020-05-26 07:11:26 +0530

    refactor url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,5 @@
 class Url < ApplicationRecord
-  before_validation :generate_shortened
+  before_validation :generate_shortened, on: [:create]
   validates :original, presence: true, uniqueness: true, format: { with: /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i }
   validates :shortened, presence: true, uniqueness: true
   belongs_to :category, optional: true
@@ -9,7 +9,7 @@ class Url < ApplicationRecord
   def generate_shortened
     loop do
       self.shortened = SecureRandom.hex(4)
-      break unless Url.exists?( shortened: self.shortened )
+      break unless Url.where( shortened: self.shortened ).exists?
     end
   end
 end
